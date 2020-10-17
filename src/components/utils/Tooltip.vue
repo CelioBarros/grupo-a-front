9ff2352232de934b2d2f57996bbc041f6c9b1e30
Célio Barros <celio.barros.filho@gmail.com>
2020-10-17 20:13:39 -0300

    add tooltip component

---
+++ src/components/utils/Tooltip.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <div v-on="on">
+        <slot name="button"></slot>
+      </div>
+    </template>
+    <slot name="text"></slot>
+  </v-tooltip>
+</template>
