e5912c55d546059a83ef3c2244cf3eb2b718fed3
Célio Barros <celio.barros.filho@gmail.com>
2020-10-17 17:46:58 -0300

    Configure vuetify and i18n

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
+    <v-app id="auth-app">
+      <v-navigation-drawer permanent>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Application
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              subtext
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense nav>
+          <v-list-item
+            v-for="item in menuItems"
+            :to="item.to"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar id="asus-top-bar" elevation="0" fixed app>
+        <v-toolbar-title>
+          <img src="@/assets/logo-grupoa.webp" alt="logo" class="pt-2" />
+        </v-toolbar-title>
+      </v-app-bar>
+    </v-app>
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      menuItems: [
+        {
+          icon: "mdi-home",
+          title: this.$t("navigation-item.home")
+        },
+        {
+          icon: "mdi-account-cog",
+          title: this.$t("navigation-item.user"),
+          to: "/"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style></style>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    i18n: {
+      locale: "pt",
+      fallbackLocale: "pt",
+      localeDir: "locales",
+      enableInSFC: false
+    }
+  }
+};
