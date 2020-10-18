9adcb5b2ff8f22189fa96e3f9924c4d999e43d02
Célio Barros <celio.barros.filho@gmail.com>
2020-10-18 13:28:59 -0300

    Add search on data table

--- src/components/user/UserDataTable.vue
+++ src/components/user/UserDataTable.vue
@@ -1,35 +1,65 @@
 <template>
-  <v-data-table
-    id="application-table"
-    :headers="headers"
-    :items="users"
-    :options.sync="options"
-    :server-items-length="totalItems"
-    class="elevation-0 transparent"
-  >
-    <template v-slot:[`item.actions`]="{ item }">
-      <div class="d-flex">
-        <tooltip>
-          <v-btn
-            :id="'btn-edit-' + item.id"
-            slot="button"
-            nuxt
-            icon
-            @click="editUser(item)"
-          >
-            <v-icon small>mdi-pencil</v-icon>
-          </v-btn>
-          <span slot="text">
-            {{ $t("components.user.data-table.tooltip.edit") }}
-          </span>
-        </tooltip>
-      </div>
-    </template>
-  </v-data-table>
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        :label="$t('components.user.data-table.search')"
+        single-line
+        hide-details
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn>
+        {{ $t("components.user.data-table.add-button") }}
+      </v-btn>
+    </v-card-title>
+    <v-data-table
+      id="application-table"
+      :headers="headers"
+      :items="users"
+      :options.sync="options"
+      :server-items-length="totalItems"
+      class="elevation-0 transparent"
+    >
+      <template v-slot:[`item.actions`]="{ item }">
+        <div class="d-inline-flex">
+          <tooltip>
+            <v-btn
+              :id="'btn-edit-' + item.id"
+              slot="button"
+              nuxt
+              icon
+              @click="editUser(item)"
+            >
+              <v-icon small>mdi-pencil</v-icon>
+            </v-btn>
+            <span slot="text">
+              {{ $t("components.user.data-table.tooltip.edit") }}
+            </span>
+          </tooltip>
+          <tooltip>
+            <v-btn
+              :id="'btn-delete-' + item.id"
+              slot="button"
+              nuxt
+              icon
+              @click="deleteUser(item)"
+            >
+              <v-icon small>mdi-delete</v-icon>
+            </v-btn>
+            <span slot="text">
+              {{ $t("components.user.data-table.tooltip.delete") }}
+            </span>
+          </tooltip>
+        </div>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import _ from "lodash";
 import Tooltip from "@/components/utils/Tooltip";
 
 export default {
@@ -41,6 +71,7 @@ export default {
     return {
       users: [],
       userSelected: {},
+      search: "",
       options: {
         size: 10,
         page: 1,
@@ -65,7 +96,7 @@ export default {
         {
           text: this.$t("components.user.data-table.header.actions"),
           sortable: false,
-
+          align: "right",
           value: "actions"
         }
       ]
@@ -77,6 +108,9 @@ export default {
         this.loadUsers();
       },
       deep: true
+    },
+    search(value) {
+      this.loadUsersDebounced();
     }
   },
   methods: {
@@ -92,7 +126,8 @@ export default {
       this.getUsers({
         page: this.options.page - 1,
         size: this.options.itemsPerPage,
-        sort
+        sort,
+        name: this.search
       })
         .then(result => {
           this.totalItems = result.data.totalElements;
@@ -102,7 +137,13 @@ export default {
           this.loading = false;
         });
     },
+    loadUsersDebounced: _.debounce(function() {
+      this.options.page === 1 ? this.loadUsers() : (this.options.page = 1);
+    }, 500),
     editUser(item) {
+      this.userSelected = item;
+    },
+    deleteUser(item) {
       this.userSelected = item;
     }
   }
