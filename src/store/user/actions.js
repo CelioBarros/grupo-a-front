787284ee672f915f6a1f0f8394878304ff2bd495
Célio Barros <celio.barros.filho@gmail.com>
2020-10-20 22:20:37 -0300

    Update action

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -8,7 +8,7 @@ export default {
     return Vue.axios.post(`users`, user);
   },
   editUser(context, user) {
-    return Vue.axios.put(`users`, user);
+    return Vue.axios.put(`users/${user.ra}`, user);
   },
   deleteUser(context, ra) {
     return Vue.axios.delete(`users/${ra}`);
