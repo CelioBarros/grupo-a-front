991a3b85a32804d2cd7d8b039431fbef23d45dbc
Célio Barros <celio.barros.filho@gmail.com>
2020-10-21 21:08:15 -0300

    Create user register dialog test

---
+++ tests/unit/components/user/UserRegisterDialog.spec.js
@@ -0,0 +1,170 @@
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import Vuex from "vuex";
+import flushPromises from "flush-promises";
+import $t from "$t";
+import UserRegisterDialog from "@/components/user/UserRegisterDialog";
+
+describe("UserRegisterDialog Component", () => {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  const stubs = {
+    VDialog: true,
+    VRow: true,
+    VCol: true,
+    VBtn: true,
+    VSpacer: true,
+    VDivider: true,
+    VCard: true,
+    VCardTitle: true,
+    VCardText: true,
+    VCardActions: true,
+    VTextField: true,
+    VForm: true,
+    VContainer: true,
+    VIcon: true
+  };
+  const store = {
+    modules: {
+      user: {
+        namespaced: true,
+        actions: {
+          saveUser: jest.fn(),
+          editUser: jest.fn()
+        }
+      }
+    }
+  };
+  const propsData = {
+    dialog: false
+  };
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mountWrapper(localVue, stubs, store, propsData);
+    wrapper.vm.$refs.form.resetValidation = jest.fn();
+    jest.clearAllMocks();
+  });
+
+  it("should register modal exists", () => {
+    expect(wrapper.find("vdialog-stub").exists()).toBeTruthy();
+  });
+
+  describe("methods", () => {
+    beforeEach(() => {
+      wrapper.vm.loading = false;
+      wrapper.vm.$refs.form.resetValidation = jest.fn();
+    });
+    describe("#close", () => {
+      it("should emit close-dialog", async () => {
+        expect(wrapper.emitted().closeDialog).toBeFalsy();
+
+        wrapper.vm.close();
+
+        await flushPromises();
+
+        expect(wrapper.emitted().closeDialog).toBeFalsy();
+      });
+      it("should call form resetValidation", async () => {
+        expect(wrapper.vm.$refs.form.resetValidation).not.toHaveBeenCalled();
+
+        wrapper.vm.close();
+
+        await flushPromises();
+
+        expect(wrapper.vm.$refs.form.resetValidation).toHaveBeenCalled();
+      });
+    });
+
+    describe("#save", () => {
+      describe("valid", () => {
+        beforeEach(() => {
+          wrapper.vm.valid = true;
+        });
+        it("should see loading", async () => {
+          expect(wrapper.vm.loading).toBeFalsy();
+
+          wrapper.vm.save();
+
+          expect(wrapper.vm.loading).toBeTruthy();
+
+          await flushPromises();
+
+          expect(wrapper.vm.loading).toBeFalsy();
+        });
+        describe("#create", () => {
+          beforeEach(() => {
+            wrapper.vm.edition = false;
+          });
+          it("should call saveUser function", async () => {
+            const saveUserSpy = jest.spyOn(wrapper.vm, "saveUser");
+            expect(saveUserSpy).not.toHaveBeenCalled();
+            wrapper.vm.save();
+
+            await flushPromises();
+
+            expect(saveUserSpy).toHaveBeenCalled();
+          });
+        });
+        describe("#edit", () => {
+          beforeEach(() => {
+            wrapper.vm.edition = true;
+          });
+          it("should call editUser function", async () => {
+            const editUserSpy = jest.spyOn(wrapper.vm, "editUser");
+            expect(editUserSpy).not.toHaveBeenCalled();
+            wrapper.vm.save();
+
+            await flushPromises();
+
+            expect(editUserSpy).toHaveBeenCalled();
+          });
+        });
+        it("should emit saved", async () => {
+          expect(wrapper.emitted().saved).toBeFalsy();
+
+          wrapper.vm.save();
+
+          await flushPromises();
+
+          expect(wrapper.emitted().saved).toBeTruthy();
+        });
+      });
+      describe("invalid", () => {
+        beforeEach(() => {
+          wrapper.vm.valid = false;
+          wrapper.vm.$refs.form.validate = jest.fn();
+        });
+        it("should call form validation", () => {
+          expect(wrapper.vm.$refs.form.validate).not.toHaveBeenCalled();
+
+          wrapper.vm.save();
+
+          expect(wrapper.vm.$refs.form.validate).toHaveBeenCalled();
+        });
+      });
+    });
+  });
+});
+
+function mountWrapper(localVue, stubs, store, propsData) {
+  return shallowMount(UserRegisterDialog, {
+    localVue,
+    stubs,
+    propsData,
+    mocks: {
+      $t,
+      $store: new Vuex.Store(store),
+      $refs: {
+        form: {
+          validate: jest.fn()
+        }
+      },
+      $vuetify: {
+        theme: {
+          dark: true
+        }
+      }
+    }
+  });
+}
